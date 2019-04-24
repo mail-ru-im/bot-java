5eebfc17b762814f195016cd0f138ddbcfa3d1b1
Daniil Popov <int02h@gmail.com>
2019-04-24 19:10:44 +0300

    Two module structure: library and sample

---
+++ library/src/main/java/ru/mail/im/botapi/BotApiClient.java
@@ -0,0 +1,8 @@
+package ru.mail.im.botapi;
+
+public class BotApiClient {
+    @Override
+    public String toString() {
+        return "botapi-client-library";
+    }
+}
---
+++ sample/src/main/java/ru/mail/im/botapi/sample/Main.java
@@ -0,0 +1,9 @@
+package ru.mail.im.botapi.sample;
+
+import ru.mail.im.botapi.BotApiClient;
+
+public class Main {
+    public static void main(final String[] args) {
+        System.out.println("Hello " + new BotApiClient());
+    }
+}
