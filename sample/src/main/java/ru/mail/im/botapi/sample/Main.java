a83239068b92c74a699388001f8dd4771518a985
Daniil Popov <int02h@gmail.com>
2019-04-30 11:53:38 +0300

    Ability to pass commands from specified file

--- sample/src/main/java/ru/mail/im/botapi/sample/Main.java
+++ sample/src/main/java/ru/mail/im/botapi/sample/Main.java
@@ -2,11 +2,17 @@ package ru.mail.im.botapi.sample;
 
 import ru.mail.im.botapi.sample.command.CommandProcessor;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
     public static void main(final String[] args) throws IOException {
-        final CommandProcessor parser = new CommandProcessor(System.in, System.out, new AppCommandListener());
+        InputStream in = System.in;
+        if (args.length == 1) {
+            in = new FileInputStream(args[0]);
+        }
+        final CommandProcessor parser = new CommandProcessor(in, System.out, new AppCommandListener());
         parser.start();
     }
 }
