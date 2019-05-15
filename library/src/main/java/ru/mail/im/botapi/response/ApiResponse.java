136736c08404abb57a7bd3eede46af0e00c3edb1
Daniil Popov <int02h@gmail.com>
2019-05-15 17:26:20 +0300

    JSON names for fields

--- library/src/main/java/ru/mail/im/botapi/response/ApiResponse.java
+++ library/src/main/java/ru/mail/im/botapi/response/ApiResponse.java
@@ -1,9 +1,14 @@
 package ru.mail.im.botapi.response;
 
+import com.google.gson.annotations.SerializedName;
+
 // TODO add to ProGuard
 public class ApiResponse {
+
+    @SerializedName("ok")
     private boolean ok;
 
     // optional, only if "ok" = false
+    @SerializedName("description")
     private String description;
 }
